c22d46e9cd45e30dde2a66f6fa7e69f6e11dbcf7
alek2535 <alekvalencia2535@gmail.com>
2023-03-09 19:42:34 -0800

    2 files modified. FInished adding navigation.

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  styleUrls: ['./product-details.component.css'],
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+  product: Product | undefined;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    // get the product id from the current route
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    // take the product id from the route and use it to find the corresponding product
+    this.product = products.find(
+      (product) => product.id === productIdFromRoute
+    );
+  }
 }
